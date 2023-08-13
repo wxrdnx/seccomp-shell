2a19ee55d86cbe3d411aed2eb9b16ef56a8986a5
wxrdnx <wxrdnx@protonmail.com>
2023-08-13 11:31:33 +0800

    implement shellcode quoted and hex

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
 use colored::Colorize;
 
+use crate::config::ScFmt;
+
 pub fn print_success(message: &str) {
     let output = format!("[{}] {}", "+".green(), message);
     println!("{}", output);
@@ -13,4 +15,18 @@ pub fn print_warning(message: &str) {
 pub fn print_error(message: &str) {
     let output = format!("[{}] {}", "+".red(), message);
     eprintln!("{}", output);
+}
+
+pub fn print_shellcode_quoted(shellcode: &[u8]) {
+    print!("\"");
+    for byte in shellcode.iter() {
+        print!("\\{:02x}", byte);
+    }
+    print!("\"");
+}
+
+pub fn print_shellcode_hex(shellcode: &[u8]) {
+    for byte in shellcode.iter() {
+        print!("{:02x}", byte);
+    }
 }
