8dfcd307cc447bd033d580c7b8f2ebcfc5baf8ef
wxrdnx <wxrdnx@protonmail.com>
2023-08-13 23:16:33 +0800

    add print_info

--- src/util.rs
+++ src/util.rs
@@ -2,18 +2,23 @@ use colored::Colorize;
 
 use crate::config::ScFmt;
 
+pub fn print_info(message: &str) {
+    let output = format!("[{}] {}", "*".blue(), message);
+    eprintln!("{}", output);
+}
+
 pub fn print_success(message: &str) {
     let output = format!("[{}] {}", "+".green(), message);
     println!("{}", output);
 }
 
 pub fn print_warning(message: &str) {
-    let output = format!("[{}] {}", "+".yellow(), message);
+    let output = format!("[{}] {}", "!".yellow(), message);
     println!("{}", output);
 }
 
 pub fn print_error(message: &str) {
-    let output = format!("[{}] {}", "+".red(), message);
+    let output = format!("[{}] {}", "-".red(), message);
     eprintln!("{}", output);
 }
 
